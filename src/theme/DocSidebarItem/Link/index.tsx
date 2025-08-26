6859212fbdaf0790420a2c579488357b7d0a62e0
Slackluky <unluckly17@gmail.com>
2025-08-26 16:26:52 +0700

    style(docs): align items vertically in doc sidebar
    This commit adjusts the flex container in the DocSidebarItemLink
    component to vertically align items. This ensures that the icon
    and text within the sidebar link are properly aligned, improving
    the visual appearance of the documentation.

--- src/theme/DocSidebarItem/Link/index.tsx
+++ src/theme/DocSidebarItem/Link/index.tsx
@@ -44,7 +44,7 @@ export default function DocSidebarItemLink({
           onClick: onItemClick ? () => onItemClick(item) : undefined,
         })}
         {...props}>
-          <div className="flex gap-2">
+          <div className="flex items-center gap-2">
           {item?.customProps?.icon && (
             <Icon icon={item.customProps.icon as string} />
           )}
