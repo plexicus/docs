f64fd2dedeaba4bdcd395c3012011094714d9d4d
Slackluky <unluckly17@gmail.com>
2025-08-26 16:26:47 +0700

    chore: disable blog plugin
    The blog plugin is not needed for the initial documentation setup.
    Disabling it simplifies the configuration and reduces unnecessary
    dependencies.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -49,6 +49,7 @@ const config: Config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
+        blog: false,
         theme: {
           customCss: './src/css/custom.css',
         },
